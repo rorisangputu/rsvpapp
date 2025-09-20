56e54d221d234b7ad2878bdbfa02e08af5072009
Rorisang Putu <rorisangputu@gmail.com>
2025-09-20 22:59:55 +0200

    creating event query for specific search params

---
+++ server/rsvp.data/Queries/EventQuery.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rsvp.data.Queries
+{
+    public class EventQuery
+    {
+        public bool? IsPrivate { get; set; }
+        public int? CategoryId { get; set; }
+        public string? SearchTerm { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+        public int Skip => (Page - 1) * PageSize;
+    }
+}
