78b9abd92e9017d733bc6411131146e127265166
Rorisang Putu <rorisangputu@gmail.com>
2025-09-20 23:42:28 +0200

    adding sorting query options

--- server/rsvp.data/Queries/EventQuery.cs
+++ server/rsvp.data/Queries/EventQuery.cs
@@ -10,6 +10,8 @@ namespace rsvp.data.Queries
         public bool? IsPrivate { get; set; }
         public int? CategoryId { get; set; }
         public string? SearchTerm { get; set; }
+        public string? SortBy { get; set; } = null;
+        public bool IsDecsending { get; set; } = false;
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 10;
 
--- server/rsvp.data/Repositories/EventRepository.cs
+++ server/rsvp.data/Repositories/EventRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using rsvp.data.Data;
 using rsvp.data.Interfaces;
 using rsvp.data.Models;
@@ -37,9 +38,37 @@ namespace rsvp.data.Repositories
             throw new NotImplementedException();
         }
 
-        public Task<List<Event>> GetAllEventsAsync(EventQuery query)
+        //Get All Events and Query filtering
+        public async Task<List<Event>> GetAllEventsAsync(EventQuery query)
         {
-            throw new NotImplementedException();
+            var events = _context.Events.AsQueryable();
+
+            if (query.CategoryId.HasValue)
+                events = events.Where(e => e.EventCategoryId == query.CategoryId.Value);
+
+
+            if (!string.IsNullOrWhiteSpace(query.SearchTerm))
+                events = events.Where(e => e.Title.Contains(query.SearchTerm));
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                events = query.SortBy.ToLower() switch
+                {
+                    "title" => events.OrderBy(e => e.Title),          // Alphabetical
+                    "created" => events.OrderByDescending(e => e.CreatedAt), // Latest first
+                    _ => events.OrderByDescending(e => e.CreatedAt) // Default fallback
+                };
+            }
+            else
+            {
+                // Default if no sort specified
+                events = events.OrderByDescending(e => e.CreatedAt);
+            }
+
+            return await events
+                .Skip(query.Skip)
+                .Take(query.PageSize)
+                .ToListAsync();
         }
 
         public Task<Event?> GetEventByIdAsync(int id)
